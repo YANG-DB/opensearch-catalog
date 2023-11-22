d628da2e2d056e1be093d8544aee21d01aa9241a
YANGDB <yang.db.dev@gmail.com>
2023-11-22 12:19:36 -0800

    update MV / Create table statements to match parquet based s3 bucket source
    Signed-off-by: YANGDB <yang.db.dev@gmail.com>

--- integrations/observability/apache/assets/tables/create_mv_apache-1.0.0.sql
+++ integrations/observability/apache/assets/tables/create_mv_apache-1.0.0.sql
@@ -1,6 +1,7 @@
 CREATE MATERIALIZED VIEW {table_name}_mview AS
 SELECT
-    to_timestamp(trim(BOTH '[]' FROM concat(time_local_1, ' ', time_local_2)), 'dd/MMM/yyyy:HH:mm:ss Z') AS `@timestamp`,
+    @timestamp AS `@timestamp`,
+    clientip as `communication.source.ip`,
     split_part (request, ' ', 1) as `http.request.method`,
     split_part (request, ' ', 2) as `http.url`,
     split_part (request, ' ', 3) as `http.flavor`,
