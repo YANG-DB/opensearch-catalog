77e59faac71b78946aadfb324a10d84a62724667
YANGDB <yang.db.dev@gmail.com>
2023-11-22 14:34:14 -0800

    update MV @timestamp column name
    Signed-off-by: YANGDB <yang.db.dev@gmail.com>

--- integrations/observability/apache/assets/tables/create_mv_apache-1.0.0.sql
+++ integrations/observability/apache/assets/tables/create_mv_apache-1.0.0.sql
@@ -1,11 +1,10 @@
 CREATE MATERIALIZED VIEW {table_name}_mview AS
 SELECT
-    '@timestamp' AS `@timestamp`,
+    `@timestamp`,
     clientip as `communication.source.ip`,
     split_part (request, ' ', 1) as `http.request.method`,
     split_part (request, ' ', 2) as `http.url`,
     split_part (request, ' ', 3) as `http.flavor`,
     status AS `http.response.status_code`,
-    body_bytes_sent AS `http.response.bytes`,
     'apache.access' AS `event.domain`
 FROM {table_name}
